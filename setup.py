9c1d436ecb2510d474a60638aba7c911d1d63393
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-02-25 19:14:10 -0300

    Version 1.3.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup
 
 setup(name='xmi2oerp',
-      version='1.3.5',
+      version='1.3.6',
       author='Cristian S. Rocha',
       author_email='cristian.rocha@moldeointeractive.com.ar',
       maintainer='Cristian S. Rocha',
