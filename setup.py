4dad31e277bd197af4d4cba25672f78ea83d8dab
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-03-01 11:00:26 -0300

    [FIX] desordered action_wfk appears in CLASS. Version 1.3.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup
 
 setup(name='xmi2oerp',
-      version='1.3.11',
+      version='1.3.12',
       author='Cristian S. Rocha',
       author_email='cristian.rocha@moldeointeractive.com.ar',
       maintainer='Cristian S. Rocha',
