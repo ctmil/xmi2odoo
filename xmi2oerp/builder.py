470c6b4dafec3afce017e5468b4a5017d6953cd0
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-05-24 10:04:36 -0300

    Use estandar all_associations method in CLASS_ASSOCIATIONS

--- xmi2oerp/builder.py
+++ xmi2oerp/builder.py
@@ -293,7 +293,7 @@ class Builder:
                     'CLASS_PARENT_NAME': parent.name if parent is not None else None,
                     'CLASS_DOCUMENTATION': ctag.get('documentation', None),
                     'CLASS_ATTRIBUTES': [ m for m in cclass.members if m.entityclass == 'cattribute' ],
-                    'CLASS_ASSOCIATIONS': [ m.swap[0] for m in cclass.associations if type(m.swap[0].participant) is uml.CClass ],
+                    'CLASS_ASSOCIATIONS': [ cclass.all_associations(ctype=uml.CClass, parents=False) ],
                     'MENU_PARENT': cclass.tag.get('menu_parent', None) or (
                         [ass.participant.tag['label']
                          for ass in cclass.associations
